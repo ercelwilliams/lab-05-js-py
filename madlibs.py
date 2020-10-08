d326ed31542988d7f2206b884930cc13fa6f5c3e
ercelwilliams <70540808+ercelwilliams@users.noreply.github.com>
2020-10-08 16:26:15 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,5 @@
+noun = input("Gimme a noun. ")
+adjective = input("How 'bout an adjective? ")
+adverb = input("Can I get an adverb? ")
+verb = input("and finally, how 'bout a verb? ")
+print("On a tropical " + noun + "," + " Underneath a " + adjective + " lava moon. " + "Hangin' " + adverb + " with the hula dancers," + verb + " questions cause' they got all the answers.")
